221ba1560c2d2adfcb2bb12cedf4ec43f80cc62c
Marcelo Sarinho <marceloactsarinho@gmail.com>
2024-03-20 13:13:08 -0300

    fix type error parcela model

--- app/Models/Parcela.php
+++ app/Models/Parcela.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Parcela extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function vendas(): BelongsTo{
+        return $this->belongsTo(Venda::class);
+    }
 }
